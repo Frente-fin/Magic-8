7b2b846c598967795a15e4ec2835aabeef964083
Joseph Long <joseph.t.long88@gmail.com>
2017-09-09 18:56:38 -0700

    more js

--- js/app.js
+++ js/app.js
@@ -3,25 +3,47 @@
 // Magic 8 returns random answer 
 
 // Define variables with querySelector 
-var newColor = document.querySelector(".circle");
+var newColor = document.querySelector(".ball");
 var thatThing = document.querySelector(".ask-question");
+var box = document.querySelector(".text-area");
+var currentValue = "Enter text";
+var getEm = document.querySelector(".output");
 var answers = ["It is certain", 
               "Only time will tell",
-              "cannot confirm or deny"];
-var randomAnswers = answers[Math.floor(Math.random())*answers.length];
+              "cannot confirm or deny",
+              "No way!",
+              "Try again",
+              "Absolutely"];
 
+
+var newText = document.querySelector(".popUp")
+
+
+function test() {
+   
+    newColor.style.background = "blue"
+}
+
+function questions() { 
+       var randomAnswers = answers[Math.floor(Math.random() * answers.length)];
+        console.log(randomAnswers);
+        getEm.innerHTML = randomAnswers;
+}
+
+
+function shakeSomething() {
+    newColor.style.webkitAnimationName = "shake";
+    
+}
+
+function makeNew() {
+    document.querySelector(".popUp").innerHTML = "Magic 8 Shake Shake"
+}
 
 
 console.log(answers.length);
 
-function test() {
-    newColor.style.backgroundColor = "blue"; 
-}
-
-function questions() {
-    console.log(randomAnswers);
-}
-
-
 thatThing.addEventListener("click", test, false);
 thatThing.addEventListener("click", questions, false);
+thatThing.addEventListener("click", shakeSomething, false);
+thatThing.addEventListener("click", makeNew, false);
